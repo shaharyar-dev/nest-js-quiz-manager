a839ab0024850f67df87730b30f46ffb669e49bf
Shaharyar <shaharyar.babar@cubixlabs.com>
2024-02-05 14:58:43 +0500

    init project

---
+++ server/src/app.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  getSomething(): string {
+    return 'Something!';
+  }
+
+}
